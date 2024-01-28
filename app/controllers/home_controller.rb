ccec9f610a9a59ad3986f1e1134b0eb4e35983b3
christinema825 <christinema0825@gmail.com>
2024-01-27 19:44:45 -0800

    fetch from api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@
 
 class HomeController < ApplicationController
     def index
-    end 
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=33.7490&lon=-84.3880&units=imperial&appid=3ab8998a4fdff1b2de0659060b4e7892"
+        uri = URI(url)
+        res = Net::HTTP.get_response(uri)
+        # debugger
+        @data = JSON.parse(res.body)
+    end
 end
